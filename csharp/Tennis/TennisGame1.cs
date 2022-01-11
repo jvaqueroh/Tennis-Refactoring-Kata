324892448ba3b39657f9c791c9f71eef2bbf21e5
jvaquero <jvaquero@aidacanarias.com>
2022-01-11 06:36:54 +0000

    Rename score variables

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,8 +2,8 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
+        private int player1Score = 0;
+        private int player2Score = 0;
         private string player1Name;
         private string player2Name;
 
@@ -16,18 +16,18 @@ namespace Tennis
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                player1Score += 1;
             else
-                m_score2 += 1;
+                player2Score += 1;
         }
 
         public string GetScore()
         {
             string score = "";
             var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (player1Score == player2Score)
             {
-                switch (m_score1)
+                switch (player1Score)
                 {
                     case 0:
                         score = "Love-All";
@@ -44,9 +44,9 @@ namespace Tennis
 
                 }
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (player1Score >= 4 || player2Score >= 4)
             {
-                var minusResult = m_score1 - m_score2;
+                var minusResult = player1Score - player2Score;
                 if (minusResult == 1) score = "Advantage player1";
                 else if (minusResult == -1) score = "Advantage player2";
                 else if (minusResult >= 2) score = "Win for player1";
@@ -56,8 +56,8 @@ namespace Tennis
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
+                    if (i == 1) tempScore = player1Score;
+                    else { score += "-"; tempScore = player2Score; }
                     switch (tempScore)
                     {
                         case 0:
